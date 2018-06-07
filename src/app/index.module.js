24a451718044eee59a5d83cf89a0dfa106909c4e
manish gautam <manish.gautam.india@gmail.com>
2018-06-07 16:22:24 +0530

    Initial checkin from Yeoman

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gogogo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
